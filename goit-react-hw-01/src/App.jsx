0bfd7541f9b097e648291392c549b723e7a27e31
Maksym <krukm187@gmail.com>
2024-02-11 11:03:34 +0200

    commit

--- goit-react-hw-01/src/App.jsx
+++ goit-react-hw-01/src/App.jsx
@@ -1,14 +1,25 @@
+import React from 'react';
+import Profile from './components/Profile';
+import FriendList from './components/FriendListItem';
+import TransactionHistory from './components/TransactionHistory';
+import userData from './userData.json';
+import friends from './friends.json';
+import transactions from './transactions.json';
 
-import './App.css'
-
-function App() {
-
-
+const App = () => {
   return (
     <>
-  
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
